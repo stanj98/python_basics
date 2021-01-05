7edf0315f3953419d8d6d3ad94eec328834aa457
Stanley John <infosoftallianze@gmail.com>
2021-01-05 08:50:41 +0530

    new concepts in python

---
+++ new_concepts.py
@@ -0,0 +1,14 @@
+'''
+Python is an interpreted language. What does that mean?
+Well, when you type in the high-level code, it will get translated into
+bytecode which is later translated into an interpreter which compiles at run-time.
+In the interpreter, it will check if the code has appropriate syntax and format only.
+As it compiles in run-time, each line is converted to machine code understood by the OS
+and executed thus everything rendered in Python is live and everything is addressed as an
+object with a memory address location.
+'''
+
+import inspect #inspect live objects
+from queue import Queue
+
+print(inspect.getsource(Queue))
